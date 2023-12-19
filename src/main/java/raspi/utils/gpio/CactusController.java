60a665faa9317c4758ece8f03a87214b8d82b2b2
Sy P <yongsa0221@gmail.com>
2023-12-19 15:54:31 +0900

    feat : gpio제어 인터페이스  추가

---
+++ src/main/java/raspi/utils/gpio/CactusController.java
@@ -0,0 +1,13 @@
+package raspi.utils;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public interface CactusController {
+
+    public void dance();
+
+    public void stop();
+
+    public void close();
+}
